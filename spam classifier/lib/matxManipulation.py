b0b6158d22ab05b316a2f861a811cd493bf98dd8
jinbel <zhuwenyu1993@163.com>
2017-06-06 21:50:43 +0800

    Spam Classifier: ps job. 2017-06-06 first commit: some basic python file: start.py lib/matxManipulation.py

---
+++ spam classifier/lib/matxManipulation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python27
+# -*- coding: utf-8 -*-
+# begin of matxManipulation.py
+
+__author__ = 'Jinbel Ju'
+
+def readMatrix(filename):
+    print filename
+
+if __name__ == '__main__':
+    print 'module matxManipulation'
+
+# end of matxManipulation.py
---
+++ spam classifier/start.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python27
+# -*- coding: utf-8 -*-
+# begin of start.py
+
+__author__ = 'Jinbel Ju'
+
+from lib import matxManipulation
+matxManipulation.readMatrix('dsdfsd')
+
+# end of start.py
